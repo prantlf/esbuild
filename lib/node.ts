98e30e3169ad1813178512eea0e6196d3d3b0cb3
Ferdinand Prantl <prantlf@gmail.com>
2020-09-16 23:54:07 +0200

    Introduce module dependency analysis without compiling the output bundle
    * Add the command-line flag `--analyse` to compute metadata of a bundle.
    * Add the `Analyse` method to the API, which produces metadata after `bundler.ScanBundle`.
    * Add the `analyse` command to the service, available only for node.
    * Include only "inputs" in metadata from the module analysis.
    
    Update internal options validation after rebasing on the latest master

--- lib/node.ts
+++ lib/node.ts
@@ -93,6 +93,14 @@ export let transform: typeof types.transform = (input, options) => {
   });
 };
 
+export let analyse: typeof types.analyse = (options: types.AnalyseOptions): any => {
+  return startService().then(service => {
+    let promise = service.analyse(options);
+    promise.then(service.stop, service.stop); // Kill the service afterwards
+    return promise;
+  });
+};
+
 export let buildSync: typeof types.buildSync = (options: types.BuildOptions): any => {
   let result: types.BuildResult;
   runServiceSync(service => service.buildOrServe('buildSync', null, options, isTTY(), (err, res) => {
@@ -128,6 +136,15 @@ export let transformSync: typeof types.transformSync = (input, options) => {
       }
     },
   }, (err, res) => {
+    if (err) throw err;
+    result = res!;
+  }));
+  return result!;
+};
+
+export let analyseSync: typeof types.analyseSync = options => {
+  let result: types.AnalyseResult;
+  runServiceSync(service => service.analyse(options, isTTY(), (err, res) => {
     if (err) throw err;
     result = res!;
   }));
@@ -196,6 +213,10 @@ export let startService: typeof types.startService = options => {
           },
         }, (err, res) => err ? reject(err) : resolve(res!)));
     },
+    analyse: (options: types.AnalyseOptions): Promise<any> =>
+      new Promise<types.AnalyseResult>((resolve, reject) =>
+        service.analyse(options, isTTY(), (err, res) =>
+          err ? reject(err) : resolve(res as types.AnalyseResult))),
     stop() { child.kill(); },
   });
 };
